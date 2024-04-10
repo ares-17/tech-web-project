ad826ff98848655dca984f4347545231c90f4206
Alessandro Schiavo <aschiavo@minsait.com>
2024-04-10 12:30:11 +0200

    removed default components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
-  <RouterView />
+  <main class="page-content content-wrapper py-5 my-3">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
