99c8d5d828920caa9aed5c9fc244cd2d7d6e7440
Alessandro Schiavo <aschiavo@minsait.com>
2024-04-10 12:03:35 +0200

    added swagger and some examples in HomeController.java

--- backend/src/main/java/com/unina/techweb/TechWebApplication.java
+++ backend/src/main/java/com/unina/techweb/TechWebApplication.java
@@ -2,8 +2,10 @@ package com.unina.techweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.unina.techweb.controller", "com.unina.techweb.repository", "com.unina.techweb.service" })
 public class TechWebApplication {
 
 	public static void main(String[] args) {
